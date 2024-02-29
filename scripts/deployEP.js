765a7352d5e96b216186244a75157f78f5cee78d
ElahiAli <ali_elahi@outlook.com>
2024-02-29 12:06:58 +0330

    add: deploy EntryPoint contract to localhost

--- scripts/deployEP.js
+++ scripts/deployEP.js
@@ -1,19 +1,19 @@
 const hre = require("hardhat");
 
 async function main() {
+    const epContract = await hre.ethers.deployContract("EntryPoint");
 
-  const epContract = await hre.ethers.deployContract("EntryPoint");
+    await epContract.waitForDeployment();
 
-  await epContract.waitForDeployment();
-
-  console.log(
-    `deployed to ${epContract.target}`
-  );
+    console.log(`deployed to ${epContract.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
+
+// hh run scripts/depoyEp.js --network localhost
+// deploying contract on localhost

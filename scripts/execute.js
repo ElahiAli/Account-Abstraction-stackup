74ba60043939da3d4b5445dcc6a3656ba4f0c5c6
ElahiAli <ali_elahi@outlook.com>
2024-02-29 14:48:54 +0330

    add: accessing to EntryPoint contract

--- scripts/execute.js
+++ scripts/execute.js
@@ -11,6 +11,7 @@ const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 async function main() {
+    const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS);
     // filling with dommy values for now
     userOp = {
         sender, //smart account address

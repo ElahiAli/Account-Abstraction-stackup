35a71aee5e1f350ed82b9f20ae759f697d92ecce
ElahiAli <ali_elahi@outlook.com>
2024-02-29 14:47:28 +0330

    add: EntyPoint contract address

--- scripts/execute.js
+++ scripts/execute.js
@@ -7,6 +7,9 @@ const FACTORY_NONCE = 1;
 // AcountFactory address in localhost
 const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
+// EntryPoint contract address in localhost
+const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 async function main() {
     // filling with dommy values for now
     userOp = {

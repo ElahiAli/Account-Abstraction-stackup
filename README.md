3f73076d3478ac391fd75bc106d94d9ec2e0651d
ElahiAli <ali_elahi@outlook.com>
2024-02-29 11:04:07 +0330

    add:  dependencies installed

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
+
+```
+using openzeppelin @4.2.0 version for EntryPoint
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,55 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+// require("solidity-coverage");
+// require("hardhat-gas-reporter");
+// require("hardhat-contract-sizer");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+            url: "http://127.0.0.1:8545",
+        },
+        // sepolia: {
+        //     url: SEPOLIA_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 11155111,
+        //     blockConfirmations: 6,
+        // },
+    },
+    // gasReporter: {
+    //     enabled: false,
+    //     currency: "USD",
+    //     outputFile: "gas-report.txt",
+    //     noColors: true,
+    //     // coinmarketcap: COINMARKETCAP_API_KEY,
+    // },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    // mocha: 200000, //200 seconds max
 };
+
+
+
+
+// GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+// PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";

1591fc97be5ca501cab0c972e5129fc5d8afbbec
ElahiAli <ali_elahi@outlook.com>
2024-03-01 19:41:49 +0330

    add: paymaster contract address

--- scripts/execute.js
+++ scripts/execute.js
@@ -10,6 +10,8 @@ const FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // EntryPoint contract address in localhost
 const EP_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
+// Paymaster contract address in localhost
+const PAYMASTER_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 async function main() {
     const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS);
     // console.log("entryPoint is fine", entryPoint);

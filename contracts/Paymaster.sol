270ce9956aacaa0506fe457d3b7e7f310451e739
ElahiAli <ali_elahi@outlook.com>
2024-03-01 19:28:26 +0330

    style: remove comments and params

--- contracts/Paymaster.sol
+++ contracts/Paymaster.sol
@@ -4,40 +4,20 @@ pragma solidity ^0.8.19;
 import "@account-abstraction/contracts/interfaces/IPaymaster.sol";
 
 contract Paymaster is IPaymaster {
-    /**
-     * payment validation: check if paymaster agrees to pay.
-     * Must verify sender is the entryPoint.
-     * Revert to reject this request.
-     * Note that bundlers will reject this method if it changes the state, unless the paymaster is trusted (whitelisted)
-     * The paymaster pre-pays using its deposit, and receive back a refund after the postOp method returns.
-     * @param userOp the user operation
-     * @param userOpHash hash of the user's request data.
-     * @param maxCost the maximum cost of this transaction (based on maximum gas and gas price from userOp)
-     * @return context value to send to a postOp
-     *      zero length to signify postOp is not required.
-     * @return validationData signature and time-range of this operation, encoded the same as the return value of validateUserOperation
-     *      <20-byte> sigAuthorizer - 0 for valid signature, 1 to mark signature failure,
-     *         otherwise, an address of an "authorizer" contract.
-     *      <6-byte> validUntil - last timestamp this operation is valid. 0 for "indefinite"
-     *      <6-byte> validAfter - first timestamp this operation is valid
-     *      Note that the validation code cannot use block.timestamp (or block.number) directly.
-     */
     function validatePaymasterUserOp(
-        UserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256 maxCost
-    ) external returns (bytes memory context, uint256 validationData);
+        UserOperation calldata,
+        bytes32,
+        uint256
+    ) external pure returns (bytes memory context, uint256 validationData) {
+        // Paymaster server alchemy?
+        // 20 bytes: paymaster address
+        // timePeriod
+        // signature
+        // userOp.paymasterAndData
 
-    /**
-     * post-operation handler.
-     * Must verify sender is the entryPoint
-     * @param mode enum with the following options:
-     *      opSucceeded - user operation succeeded.
-     *      opReverted  - user op reverted. still has to pay for gas.
-     *      postOpReverted - user op succeeded, but caused postOp (in mode=opSucceeded) to revert.
-     *                       Now this is the 2nd call, after user's op was deliberately reverted.
-     * @param context - the context value returned by validatePaymasterUserOp
-     * @param actualGasCost - actual gas used so far (without this postOp call).
-     */
-    function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) external;
+        context = new bytes(0); // we dont send any context
+        validationData = 0; // means that signature is valid and we will pay for the gas
+    }
+
+    function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) external {}
 }

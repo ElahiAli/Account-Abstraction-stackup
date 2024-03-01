37b3fdf404d0987e7093be5b4bbe9bbf9fc85b95
ElahiAli <ali_elahi@outlook.com>
2024-03-01 19:39:02 +0330

    edit: local address changed

--- scripts/execute.js
+++ scripts/execute.js
@@ -5,10 +5,10 @@ const hre = require("hardhat");
 const FACTORY_NONCE = 1;
 
 // AcountFactory address in localhost
-const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // EntryPoint contract address in localhost
-const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const EP_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 async function main() {
     const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS);

3a71100ee8621d298daeedfe827d58656a4d6b49
ElahiAli <ali_elahi@outlook.com>
2024-03-05 15:07:32 +0330

    add: using stackup simple example

---
+++ stackup/erc-4337/scripts/init.ts
@@ -0,0 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
+import prettier from "prettier";
+import { ethers } from "ethers";
+import { Presets } from "userop";
+
+const INIT_CONFIG = {
+  rpcUrl: "https://api.stackup.sh/v1/node/API_KEY",
+  signingKey: new ethers.Wallet(ethers.utils.randomBytes(32)).privateKey,
+  secp256r1Key: Presets.Signers.BarzSecp256r1.generatePrivateKey(),
+  paymaster: {
+    rpcUrl: "https://api.stackup.sh/v1/paymaster/API_KEY",
+    context: {},
+  },
+};
+const CONFIG_PATH = path.resolve(__dirname, "../config.json");
+
+async function main() {
+  return fs.writeFile(
+    CONFIG_PATH,
+    prettier.format(JSON.stringify(INIT_CONFIG, null, 2), { parser: "json" })
+  );
+}
+
+main()
+  .then(() => console.log(`Config written to ${CONFIG_PATH}`))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

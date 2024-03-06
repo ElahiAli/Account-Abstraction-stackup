e34788cc8b4b89e3e2c2341ac58b889d3be6c7d3
ElahiAli <ali_elahi@outlook.com>
2024-03-06 15:41:55 +0330

    add: add poll contract interface

---
+++ contracts/IPool.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+interface IPool {
+    function setOwnerTokenShare(address ownerAddress, uint kipTokenAmount) external;
+
+    function withdraw() external;
+
+    function getPoolBalance() external view returns (uint);
+
+    function getKipTokenAddress() external view returns (address);
+
+    function getOwnerBalance(address ownerAddress) external view returns (uint);
+}

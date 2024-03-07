718b8df326e41ca33b860b091444632f81596f1c
ElahiAli <ali_elahi@outlook.com>
2024-03-07 10:04:54 +0330

    add: main function

---
+++ stackup/kip-4337/main.ts
@@ -0,0 +1,8 @@
+import {ethers} from "ethers";
+import { Presets, Client } from "userop";
+
+async function main() {
+    // REMAINDER OF CODE WILL GO HERE
+  }
+  
+  main().catch((err) => console.error("Error:", err));

cd0eb41c51f0fe5cb30cce2a16dcb666b431a0b4
ElahiAli <ali_elahi@outlook.com>
2024-03-07 10:27:41 +0330

    add: paymaster initialize a paymaster

--- stackup/kip-4337/main.ts
+++ stackup/kip-4337/main.ts
@@ -1,8 +1,21 @@
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 import { Presets, Client } from "userop";
 
+const rpcUrl = "https://public.stackup.sh/api/v1/node/ethereum-sepolia";
+const paymasterUrl = ""; // Optional - you can get one at https://app.stackup.sh/
+
 async function main() {
-    // REMAINDER OF CODE WILL GO HERE
-  }
-  
-  main().catch((err) => console.error("Error:", err));
+    const paymasterContext = { type: "payg" };
+    const paymasterMiddleware = Presets.Middleware.verifyingPaymaster(
+        paymasterUrl,
+        paymasterContext
+    );
+    const opts =
+        paymasterUrl === ""
+            ? {}
+            : {
+                  paymasterMiddleware: paymasterMiddleware,
+              };
+}
+
+main().catch((err) => console.error("Error:", err));

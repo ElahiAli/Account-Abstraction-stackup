fafb6bbd16eee63f09e52053ac3cb07bbfb3d99e
ElahiAli <ali_elahi@outlook.com>
2024-03-08 12:27:18 +0330

    update: creating a batch user op with erc-20 (approve and transfer)

--- stackup/kip-4337/main.ts
+++ stackup/kip-4337/main.ts
@@ -1,3 +1,5 @@
+// Enter `npm run dev` into your terminal to run.
+
 import { ethers } from "ethers";
 import { Presets, Client } from "userop";
 
@@ -11,7 +13,7 @@ async function main() {
         paymasterContext
     );
     const opts =
-        paymasterUrl === ""
+        paymasterUrl.toString() === ""
             ? {}
             : {
                   paymasterMiddleware: paymasterMiddleware,
@@ -25,23 +27,36 @@ async function main() {
     console.log(`Account address: ${address}`);
 
     // Create the call data
+    const to = address; // Receiving address, in this case we will send it to ourselves
+    const token = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B"; // Address of the ERC-20 token
+    const value = "0"; // Amount of the ERC-20 token to transfer
 
-    const aaAdress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"; // Address of the Account Abstraction
-
-    // Read the Account Abstraction contract
+    // Read the ERC-20 token contract
+    const ERC20_ABI = require("./erc20Abi.json"); // ERC-20 ABI in json format
     const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-    const AccountAbstraction_ABI = require("./AccountAbstraction.json"); // Account-Abstraction ABI in json format
-    const AAContract = new ethers.Contract(aaAdress, AccountAbstraction_ABI, provider);
+    const erc20 = new ethers.Contract(token, ERC20_ABI, provider);
+    const decimals = await Promise.all([erc20.decimals()]);
+    const amount = ethers.utils.parseUnits(value, decimals);
 
     // Encode the calls
-    const callTo = [aaAdress, aaAdress];
-    const callData = [AAContract.interface.encodeFunctionData("getAccountAbstractionBalance")];
+    const callTo = [token, token];
+    const callData = [
+        erc20.interface.encodeFunctionData("approve", [to, amount]),
+        erc20.interface.encodeFunctionData("transfer", [to, amount]),
+    ];
 
     // Send the User Operation to the ERC-4337 mempool
     const client = await Client.init(rpcUrl);
     const res = await client.sendUserOperation(builder.executeBatch(callTo, callData), {
         onBuild: (op) => console.log("Signed UserOperation:", op),
     });
+
+    // Return receipt
+    console.log(`UserOpHash: ${res.userOpHash}`);
+    console.log("Waiting for transaction...");
+    const ev = await res.wait();
+    console.log(`Transaction hash: ${ev?.transactionHash ?? null}`);
+    console.log(`View here: https://jiffyscan.xyz/userOpHash/${res.userOpHash}`);
 }
 
 main().catch((err) => console.error("Error:", err));

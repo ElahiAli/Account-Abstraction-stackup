7b20d5c870b2ae752e8e5248a2f0a508bba56f46
ElahiAli <ali_elahi@outlook.com>
2024-03-12 11:14:40 +0330

    edit: change contract to a simple AA mock

--- contracts/AccountKip.sol
+++ contracts/AccountKip.sol
@@ -8,43 +8,40 @@ contract AccountAbstraction {
     IERC20 kipToken;
     address private poolContractAddress;
     IPool poolContract;
-    address public immutable APP_OWNER_ADDRESS;
-    address public immutable Model_OWNER_ADDRESS;
-    address public immutable DATASET_OWNER_ADDRESS;
-    uint public APP_SHARE = 50;
-    uint public MODEL_SHARE = 30;
-    uint public DATASET_SHARE = 20;
+    // address public immutable APP_OWNER_ADDRESS;
+    // address public immutable Model_OWNER_ADDRESS;
+    // address public immutable DATASET_OWNER_ADDRESS;
+    // uint public APP_SHARE = 50;
+    // uint public MODEL_SHARE = 30;
+    // uint public DATASET_SHARE = 20;
 
     mapping(address => uint) private s_userEscrowedToken;
 
-    constructor(
-        address _poolContractAddress,
-        address _tokenAddress,
-        address _appOwner,
-        address _modelOwner,
-        address _dataOwner
-    ) {
+    constructor(address _poolContractAddress, address _tokenAddress) // address _appOwner,
+    // address _modelOwner,
+    // address _dataOwner
+    {
         poolContractAddress = _poolContractAddress;
-        poolContract = IPool(poolContractAddress);
+        // poolContract = IPool(poolContractAddress);
         kipToken = IERC20(_tokenAddress);
-        APP_OWNER_ADDRESS = _appOwner;
-        Model_OWNER_ADDRESS = _modelOwner;
-        DATASET_OWNER_ADDRESS = _dataOwner;
+        // APP_OWNER_ADDRESS = _appOwner;
+        // Model_OWNER_ADDRESS = _modelOwner;
+        // DATASET_OWNER_ADDRESS = _dataOwner;
     }
 
     //token must be transferred to pool contract
     function escrowedTokens(uint amount) public {
         kipToken.transferFrom(msg.sender, poolContractAddress, amount);
-        amount = amount * 10 ** 6;
-        poolContract.setOwnerTokenShare(APP_OWNER_ADDRESS, ((amount * APP_SHARE) / 100) / 10 ** 6);
-        poolContract.setOwnerTokenShare(
-            Model_OWNER_ADDRESS,
-            ((amount * MODEL_SHARE) / 100) / 10 ** 6
-        );
-        poolContract.setOwnerTokenShare(
-            APP_OWNER_ADDRESS,
-            ((amount * DATASET_SHARE) / 100) / 10 ** 6
-        );
+        // amount = amount * 10 ** 6;
+        // poolContract.setOwnerTokenShare(APP_OWNER_ADDRESS, ((amount * APP_SHARE) / 100) / 10 ** 6);
+        // poolContract.setOwnerTokenShare(
+        //     Model_OWNER_ADDRESS,
+        //     ((amount * MODEL_SHARE) / 100) / 10 ** 6
+        // );
+        // poolContract.setOwnerTokenShare(
+        //     APP_OWNER_ADDRESS,
+        //     ((amount * DATASET_SHARE) / 100) / 10 ** 6
+        // );
     }
 
     function withdraw() public {

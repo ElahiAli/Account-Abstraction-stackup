698fb0c12c1049eb4e0b3f02b917297bd561de93
ElahiAli <ali_elahi@outlook.com>
2024-03-12 11:15:21 +0330

    edit: script for deploying mock AA contract

--- deploy/DeployAccountAbstraction.js
+++ deploy/DeployAccountAbstraction.js
@@ -1,7 +1,14 @@
 const hre = require("hardhat");
-const appOwner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-const modelOwner = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
-const dataOwner = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+// const appOwner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+// const modelOwner = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+// const dataOwner = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+
+const poolContractAddress = "0x06Ada51C0759B65fdbBc5D3305DF563C5fc6329E";
+const kipTokenAddress = "0x01907E8215e48809E1E06083294fCb5FC74296f5";
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
@@ -11,36 +18,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     log("-------------------------------------------");
 
-    const kipToken = await deploy("KipMock", {
-        from: deployer,
-        args: [],
-        log: true,
-        // waitConfirmations: waitBlockConfirmations,
-    });
-
-    log("-------------------------------------------");
-
-    const poolContract = await deploy("PoolMock", {
-        from: deployer,
-        args: [],
-        log: true,
-        // waitConfirmations: waitBlockConfirmations,
-    });
-
-    log("-------------------------------------------");
-
-    const arguments = [poolContract.address, kipToken.address, appOwner, modelOwner, dataOwner];
+    const arguments = [poolContractAddress, kipTokenAddress];
     const accountAbstraction = await deploy("AccountAbstraction", {
         from: deployer,
         args: arguments,
         log: true,
         // waitConfirmations: waitBlockConfirmations,
     });
-    // if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-    //     log("Verifying...");
-    //     await verify(accountAbstraction.address, arguments);
-    //     log("Verified!");
-    // }
+    console.log("Account Deployed.");
+
+    log("-------------------------------------------");
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...");
+        await verify(accountAbstraction.address, arguments);
+        log("Verified!");
+    }
     log("----------------------------------------------------");
 };
 
